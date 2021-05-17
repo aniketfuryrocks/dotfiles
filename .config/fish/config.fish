c0bb3cd97cda5af3cb5620ca615a61ba49c86559
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-05-17 17:30:55 +0530

    nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,10 @@
 alias l="exa -la"
 alias dir="ranger"
+
+# env
+set EDITOR "nvim"
+
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+
+string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
+
