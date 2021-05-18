c3710110364447fb411b2d4302e85ea58723fabb
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-05-18 17:51:29 +0530

    path to local bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,6 @@ alias dir="ranger"
 set EDITOR "nvim"
 
 set -gx WASMTIME_HOME "$HOME/.wasmtime"
-
+set PATH "$HOME/.local/bin:$PATH"
 string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
 
