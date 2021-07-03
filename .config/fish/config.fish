c07b45690f437411d2f2e37ff4ebc927a7c200ae
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-07-03 15:08:36 +0530

    deno

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,5 +10,7 @@ set EDITOR "nvim"
 set -gx WASMTIME_HOME "$HOME/.wasmtime"
 set PATH "$HOME/.local/bin:$PATH"
 set PATH "/opt/android_sdk/platform-tools:$PATH"
+set DENO_INSTALL "$HOME/.deno"
+set PATH "$DENO_INSTALL/bin:$PATH"
 string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
 
