93eaecfa2caa7d1fd70aac39d461b68a6c89d407
aniketfuryrocks <contact@aniketprajapati.me>
2022-06-23 12:03:08 +0530

    new dev_toolbox

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/var/home/aniket/.toolbox/homedir/dev/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/var/home/aniket/.toolbox/homedir/dev/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/var/home/aniket/.toolbox/homedir/dev/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/var/home/aniket/.toolbox/homedir/dev/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/var/home/aniket/.toolbox/homedir/dev/aniket/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,194 +71,194 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["alpha-nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/alpha-nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/alpha-nvim",
     url = "https://github.com/goolord/alpha-nvim"
   },
   ["bufferline.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   ["cmp-vsnip"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
     url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["crates.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/crates.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/crates.nvim",
     url = "https://github.com/saecki/crates.nvim"
   },
   ["fidget.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/fidget.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/fidget.nvim",
     url = "https://github.com/j-hui/fidget.nvim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["glow.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/glow.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/glow.nvim",
     url = "https://github.com/npxbr/glow.nvim"
   },
   ["glslView-nvim"] = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/opt/glslView-nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/opt/glslView-nvim",
     url = "https://github.com/timtro/glslView-nvim"
   },
   ["grammar-guard.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/grammar-guard.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/grammar-guard.nvim",
     url = "https://github.com/brymer-meneses/grammar-guard.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["material.vim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/material.vim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/material.vim",
     url = "https://github.com/kaicataldo/material.vim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-dap"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-dap",
     url = "https://github.com/mfussenegger/nvim-dap"
   },
   ["nvim-dap-ui"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-dap-ui",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-dap-ui",
     url = "https://github.com/rcarriga/nvim-dap-ui"
   },
   ["nvim-dap-virtual-text"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text",
     url = "https://github.com/theHamsta/nvim-dap-virtual-text"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-notify"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-notify",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-notify",
     url = "https://github.com/rcarriga/nvim-notify"
   },
   ["nvim-terminal"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-terminal",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-terminal",
     url = "https://github.com/s1n7ax/nvim-terminal"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["rust-tools.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/rust-tools.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/rust-tools.nvim",
     url = "https://github.com/simrat39/rust-tools.nvim"
   },
   ["spellsitter.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/spellsitter.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/spellsitter.nvim",
     url = "https://github.com/lewis6991/spellsitter.nvim"
   },
   ["telescope-dap.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/telescope-dap.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/telescope-dap.nvim",
     url = "https://github.com/nvim-telescope/telescope-dap.nvim"
   },
   ["telescope-ui-select.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/telescope-ui-select.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/telescope-ui-select.nvim",
     url = "https://github.com/nvim-telescope/telescope-ui-select.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["todo-comments.nvim"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/todo-comments.nvim",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/todo-comments.nvim",
     url = "https://github.com/folke/todo-comments.nvim"
   },
   ["vim-glsl"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/vim-glsl",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/vim-glsl",
     url = "https://github.com/tikhomirov/vim-glsl"
   },
   ["vim-rooter"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/vim-rooter",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/vim-rooter",
     url = "https://github.com/airblade/vim-rooter"
   },
   ["vim-visual-multi"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/vim-visual-multi",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/vim-visual-multi",
     url = "https://github.com/mg979/vim-visual-multi"
   },
   ["vim-vsnip"] = {
     loaded = true,
-    path = "/home/aniket/.local/share/nvim/site/pack/packer/start/vim-vsnip",
+    path = "/var/home/aniket/.toolbox/homedir/dev/aniket/.local/share/nvim/site/pack/packer/start/vim-vsnip",
     url = "https://github.com/hrsh7th/vim-vsnip"
   }
 }
