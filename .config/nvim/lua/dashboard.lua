fae18efd586e8ffc32493d3740630a83df615418
aniketfuryrocks <contact@aniketprajapati.me>
2022-07-20 11:11:01 +0530

    r

--- .config/nvim/lua/dashboard.lua
+++ .config/nvim/lua/dashboard.lua
@@ -14,7 +14,7 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Open Projects", ":cd /mnt/Projects | RooterToggle | Telescope find_files <CR>"),
+    dashboard.button( "f", "  > Open Projects", ":cd /mnt/Projects | Telescope find_files <CR>"),
     dashboard.button( "n", "  > Notes", ":cd $HOME/Nextcloud/Notes | NvimTreeOpen <CR>"),
     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
