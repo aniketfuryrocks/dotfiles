6a8cadec2c832e65ebf4961f9b865bef2d6c0812
aniketfuryrocks <contact@aniketprajapati.me>
2024-03-02 14:39:28 +0530

    a

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,25 +10,25 @@ set --export ANDROID_HOME "$HOME/Library/Android/sdk"
 set --export BUN_INSTALL "$HOME/.bun"
 
 # tools and language
-set --export PATH "$HOME/.local/bin:$PATH"
-set --export PATH "$HOME/.cargo/bin:$PATH"
-set --export PATH "$HOME/go/bin:$PATH"
-set --export PATH "$HOME/.yarn/bin:$PATH"
-set --export PATH "$HOME/.flutter/bin:$PATH"
-set --export PATH "$HOME/.foundry/bin:$PATH"
-set --export PATH "$DENO_INSTALL/bin:$PATH"
-set --export PATH "$HOME/.local/share/solana/install/active_release/bin:$PATH"
-set --export PATH "$BUN_INSTALL/bin:$PATH"
+set  PATH "$HOME/.local/bin:$PATH"
+set  PATH "$HOME/.cargo/bin:$PATH"
+set  PATH "$HOME/go/bin:$PATH"
+set  PATH "$HOME/.yarn/bin:$PATH"
+set  PATH "$HOME/.flutter/bin:$PATH"
+set  PATH "$HOME/.foundry/bin:$PATH"
+set  PATH "$DENO_INSTALL/bin:$PATH"
+set  PATH "$HOME/.local/share/solana/install/active_release/bin:$PATH"
+set  PATH "$BUN_INSTALL/bin:$PATH"
 
 # android tools
-set --export PATH "$ANDROID_HOME/platform-tools:$PATH"
+set  PATH "$ANDROID_HOME/platform-tools:$PATH"
 
 # ruby
-set --export PATH "/opt/homebrew/opt/ruby/bin:$PATH"
+set  PATH "/opt/homebrew/opt/ruby/bin:$PATH"
 
 # Homebrew
-set --export PATH "/opt/homebrew/bin:$PATH"
-set --export PATH "/opt/homebrew/sbin:$PATH"
+set  PATH "/opt/homebrew/bin:$PATH"
+set  PATH "/opt/homebrew/sbin:$PATH"
 
 # For compilers to find ruby you may need to set:
 set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib"
@@ -56,3 +56,4 @@ alias dir="ranger"
 alias ports="sudo lsof -i -P -n | grep LISTEN"
 alias edit="neovide --multigrid" 
 alias notes="edit ~/Nextcloud/Notes"
+alias pinentry="pinentry-mac"
