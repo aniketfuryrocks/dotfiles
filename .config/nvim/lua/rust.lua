212f6273533a08ac155ad9abe67a49c5d567fcd7
aniketfuryrocks <contact@aniketprajapati.me>
2025-03-06 21:17:07 +0530

    a

--- .config/nvim/lua/rust.lua
+++ .config/nvim/lua/rust.lua
@@ -4,8 +4,15 @@ local cfg = require('rustaceanvim.config')
 local codelldb_path = "/Users/aniketprajapati/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/adapter/codelldb"
 local liblldb_path = "/Users/aniketprajapati/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/lldb/lib/liblldb.dylib"
 
+-- Generate a random temp directory
+local function get_random_tmp_dir()
+    local tmp_dir = "/tmp/rust-analyzer-" .. tostring(math.random(100000, 999999))
+    vim.fn.mkdir(tmp_dir, "p")  -- Create directory if not exists
+    return tmp_dir
+end
+
 vim.g.rustaceanvim = {
-    tools = { -- rust-tools options
+    tools = {
         autoSetHints = true,
         inlay_hints = {
             show_parameter_hints = false,
@@ -29,10 +36,10 @@ vim.g.rustaceanvim = {
                 },
                 checkOnSave = {
                     command = "clippy",
-                    extraArgs = {
-                        "--target-dir",
-                        "/tmp/rust-analyzer-check"
-                    }
+                    -- extraArgs = {
+                    --     "--target-dir",
+                    --     get_random_tmp_dir()  -- Use dynamically created temp dir
+                    -- }
                 },
             }
         },
